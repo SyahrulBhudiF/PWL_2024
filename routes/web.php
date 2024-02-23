71f423094c4e7b938f35d67433a360fda841c82e
Syahrul <syahrul4w@gmail.com>
2024-02-23 21:09:40 +0700

    feat: add Parameters Route

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,16 @@ Route::get('/world', function () {
 Route::get('/about', function () {
     return '2241720167/Syahrul Bhudi Ferdiansyah';
 });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel
+    dengan ID {$id}";
+});
